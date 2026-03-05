e01b8b1ea6bd1d1d6fea4de450c7cd832f43eca3
Fergi3 <jakeferguson333@outlook.com>
2026-03-05 15:23:12 -0600

    feat: multidimensional menu/refactored for a switch

--- Program.cs
+++ Program.cs
@@ -1,106 +1,169 @@
-﻿﻿using NLog;
+using NLog;
 using System.Reflection;
 using System.Text.Json;
-string path = Directory.GetCurrentDirectory() + "//nlog.config";
 
-// create instance of Logger
+string path = Directory.GetCurrentDirectory() + "//nlog.config";
 var logger = LogManager.Setup().LoadConfigurationFromFile(path).GetCurrentClassLogger();
 
 logger.Info("Program started");
 
-// deserialize mario json from file into List<Mario>
 string marioFileName = "mario.json";
-List<Mario> marios = [];
-// check if file exists
-if (File.Exists(marioFileName))
-{
-  marios = JsonSerializer.Deserialize<List<Mario>>(File.ReadAllText(marioFileName))!;
-  logger.Info($"File deserialized {marioFileName}");
-}
+string donkeyKongFileName = "dk.json";
+string streetFighterFileName = "sf2.json";
 
-do
+List<Mario> marios = LoadCharacters<Mario>(marioFileName, logger);
+List<DonkeyKong> donkeyKongs = LoadCharacters<DonkeyKong>(donkeyKongFileName, logger);
+List<StreetFighter> streetFighters = LoadCharacters<StreetFighter>(streetFighterFileName, logger);
+
+while (true)
 {
-  // display choices to user
-  Console.WriteLine("1) Display Mario Characters");
-  Console.WriteLine("2) Add Mario Character");
-  Console.WriteLine("3) Remove Mario Character");
+  Console.WriteLine("Choose a game:");
+  Console.WriteLine("1) Mario");
+  Console.WriteLine("2) Donkey Kong");
+  Console.WriteLine("3) Street Fighter II");
   Console.WriteLine("Enter to quit");
 
-  // input selection
-  string? choice = Console.ReadLine();
-  logger.Info("User choice: {Choice}", choice);
+  string? gameChoice = Console.ReadLine();
+  logger.Info("Game choice: {Choice}", gameChoice);
 
-  if (choice == "1")
+  if (string.IsNullOrEmpty(gameChoice))
   {
-    // Display Mario Characters
-     foreach(var c in marios)
-    {
-      Console.WriteLine(c.Display());
-    }
-  }
-  else if (choice == "2")
-  {
-    // Add Mario Character
-     // Generate unique Id
-    Mario mario = new()
-    {
-      Id = marios.Count == 0 ? 1 : marios.Max(c => c.Id) + 1
-    };
-    InputCharacter(mario);
-     // Add Character
-    marios.Add(mario);
-    File.WriteAllText(marioFileName, JsonSerializer.Serialize(marios));
-    logger.Info($"Character added: {mario.Name}");
-  }
-  else if (choice == "3")
-  {
-    // Remove Mario Character
-       Console.WriteLine("Enter the Id of the character to remove:");
-    if (UInt32.TryParse(Console.ReadLine(), out UInt32 Id))
-    {
-      Mario? character = marios.FirstOrDefault(c => c.Id == Id);
-      if (character == null)
-      {
-        logger.Error($"Character Id {Id} not found");
-      } else {
-         marios.Remove(character);
-        // serialize list<marioCharacter> into json file
-        File.WriteAllText(marioFileName, JsonSerializer.Serialize(marios));
-        logger.Info($"Character Id {Id} removed");
-      }
-    } else {
-      logger.Error("Invalid Id");
-    }
-  } else if (string.IsNullOrEmpty(choice)) {
     break;
-  } else {
-    logger.Info("Invalid choice");
   }
-} while (true);
+
+  Console.WriteLine("Choose an action:");
+  Console.WriteLine("1) Display Characters");
+  Console.WriteLine("2) Add Character");
+  Console.WriteLine("3) Remove Character");
+  Console.WriteLine("4) Back");
+
+  string? actionChoice = Console.ReadLine();
+  logger.Info("Action choice: {Choice}", actionChoice);
+
+  switch (gameChoice)
+  {
+    case "1":
+      HandleAction(actionChoice, marios, marioFileName, logger);
+      break;
+    case "2":
+      HandleAction(actionChoice, donkeyKongs, donkeyKongFileName, logger);
+      break;
+    case "3":
+      HandleAction(actionChoice, streetFighters, streetFighterFileName, logger);
+      break;
+    default:
+      logger.Info("Invalid game choice");
+      break;
+  }
+}
 
 logger.Info("Program ended");
+
+static List<T> LoadCharacters<T>(string fileName, Logger logger) where T : Character
+{
+  if (!File.Exists(fileName))
+  {
+    return [];
+  }
+
+  List<T>? characters = JsonSerializer.Deserialize<List<T>>(File.ReadAllText(fileName));
+  logger.Info("File deserialized {FileName}", fileName);
+  return characters ?? [];
+}
+
+static void DisplayCharacters<T>(List<T> characters) where T : Character
+{
+  foreach (T character in characters)
+  {
+    Console.WriteLine(character.Display());
+  }
+}
+
+static void HandleAction<T>(string? actionChoice, List<T> characters, string fileName, Logger logger)
+  where T : Character, new()
+{
+  switch (actionChoice)
+  {
+    case "1":
+      DisplayCharacters(characters);
+      break;
+    case "2":
+      AddCharacter(characters, fileName, logger);
+      break;
+    case "3":
+      RemoveCharacter(characters, fileName, logger);
+      break;
+    case "4":
+      break;
+    default:
+      logger.Info("Invalid action choice");
+      break;
+  }
+}
+
+static void AddCharacter<T>(List<T> characters, string fileName, Logger logger) where T : Character, new()
+{
+  T character = new()
+  {
+    Id = characters.Count == 0 ? 1 : characters.Max(c => c.Id) + 1
+  };
+
+  InputCharacter(character);
+  characters.Add(character);
+  File.WriteAllText(fileName, JsonSerializer.Serialize(characters));
+  logger.Info("Character added: {Name}", character.Name);
+}
+
+static void RemoveCharacter<T>(List<T> characters, string fileName, Logger logger) where T : Character
+{
+  Console.WriteLine("Enter the Id of the character to remove:");
+
+  if (!UInt64.TryParse(Console.ReadLine(), out UInt64 id))
+  {
+    logger.Error("Invalid Id");
+    return;
+  }
+
+  T? character = characters.FirstOrDefault(c => c.Id == id);
+  if (character == null)
+  {
+    logger.Error("Character Id {Id} not found", id);
+    return;
+  }
+
+  characters.Remove(character);
+  File.WriteAllText(fileName, JsonSerializer.Serialize(characters));
+  logger.Info("Character Id {Id} removed", id);
+}
 
 static void InputCharacter(Character character)
 {
   Type type = character.GetType();
   PropertyInfo[] properties = type.GetProperties();
-  var props = properties.Where(p => p.Name != "Id");
+  IEnumerable<PropertyInfo> props = properties.Where(p => p.Name != "Id");
+
   foreach (PropertyInfo prop in props)
   {
     if (prop.PropertyType == typeof(string))
     {
       Console.WriteLine($"Enter {prop.Name}:");
       prop.SetValue(character, Console.ReadLine());
-    } else if (prop.PropertyType == typeof(List<string>)) {
+    }
+    else if (prop.PropertyType == typeof(List<string>))
+    {
       List<string> list = [];
-      do {
+      do
+      {
         Console.WriteLine($"Enter {prop.Name} or (enter) to quit:");
         string response = Console.ReadLine()!;
-        if (string.IsNullOrEmpty(response)){
+        if (string.IsNullOrEmpty(response))
+        {
           break;
         }
+
         list.Add(response);
       } while (true);
+
       prop.SetValue(character, list);
     }
   }
